41f04fe7e6ce23d653b3551c012e55363a2e13ae
Akshay Agarwal <aagrawal6372@altimetrik.com>
2020-05-26 15:57:16 +0530

    Updates

---
+++ precache-manifest.70d65b293331fd2ce1238324e6469075.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d563eb926d2f0a8801e08963b44db074",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "b8fd5c64cd2099040920",
+    "url": "/weather-app/static/css/main.a2b08d32.chunk.css"
+  },
+  {
+    "revision": "6f9da207b79e184da8ff",
+    "url": "/weather-app/static/js/2.095184d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.095184d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8fd5c64cd2099040920",
+    "url": "/weather-app/static/js/main.efa72204.chunk.js"
+  },
+  {
+    "revision": "12ef64cf8e1aad5a2dd2",
+    "url": "/weather-app/static/js/runtime-main.c90f5b59.js"
+  },
+  {
+    "revision": "a22f258357798b97bb4811b675970838",
+    "url": "/weather-app/static/media/orange-blue-cloudy-sky-159020.a22f2583.jpg"
+  },
+  {
+    "revision": "8924161871aa5ccf66975f146a18a66d",
+    "url": "/weather-app/static/media/photography-of-fall-trees-1591447.89241618.jpg"
+  }
+]);
